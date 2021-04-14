a32db97e841afc76a82644f132edce7b2063f065
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-14 14:06:30 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// console.log('JS Ok');
+// console.log('JQ', $);
+
+/*******
+ * TODO APP
+ */
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+    // End Doc Ready
+})
+
+
+
+
+/**************************
+ * 
+ * UTILITIES / FUNCTIONS
+ * 
+ **************************/
